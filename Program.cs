c2176d928192c6fee74e968f2917ab08462bc9d3
İREM İÇYER <62353089+iremicyer@users.noreply.github.com>
2020-08-10 10:54:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Drawing;
+using System.Net.Http.Headers;
+
+namespace RandomİkiBoyutlu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("satir sayisini giriniz:"); // kullancııdan sütun sayisinin alinmasi
+            int satir = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("sütun sayisi giriniz:"); // kuullanıcıdan satir sayisinin alinmasi
+            int sütun = Convert.ToInt32(Console.ReadLine());
+
+          
+
+            Random rnd = new Random(); // random sayi üretmek için fonksiyon oluşturulması
+
+            int[,] dizi = new int[sütun, satir]; // iki boyutlu dizi tanimlanmasi
+
+            for (int i = 0;  i< sütun; i++)  
+            {
+                for (int j = 0; j < satir; j++)
+                {
+                    dizi[i,j] = rnd.Next(0,100);
+                    Console.Write(dizi[i,j] +"\t" );
+
+                     //Console.Write(random[i, j] + " \t"); //  \t dizide sağa doğru bir tab boşulk bırakılmasını sağlar.
+
+                   
+                }
+                Console.WriteLine("\n"); // girilen boyutlara göre dizisinin aşağı doğru devam ettirilmesi.
+
+           
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
